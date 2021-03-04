c49ed5508d19553f31bed319025bb10457f9fa92
Zhiyao Li <l709319582@gmail.com>
2021-03-04 13:45:19 +1100

    [Init] Project Initialization

---
+++ vicflora-frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="header">
+    <Header></Header>
+  </div>
+  <router-view />
+</template>
+
+<style lang="scss" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
+
+<script>
+import Header from "./views/Header/Header";
+export default {
+  components: {
+    Header,
+  },
+}
+</script>
---
+++ vicflora-frontend/src/views/Header/Header.vue
@@ -0,0 +1,11 @@
+<style scoped>
+
+</style>
+<template>
+    <div>Header</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
