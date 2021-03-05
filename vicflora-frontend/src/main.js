92c45478e1810296572c484479bac35b22016ea6
Zhiyao Li <l709319582@gmail.com>
2021-03-05 11:50:29 +1100

    [Enhance] Add BootstrapVue as UI framework

--- vicflora-frontend/src/main.js
+++ vicflora-frontend/src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+//import Bootstrap as UI framework
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false
 
 new Vue({
