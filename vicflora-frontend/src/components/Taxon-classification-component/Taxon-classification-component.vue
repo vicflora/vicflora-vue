861738050e81493f624d019cf3f2e0e1c4149e12
toraycaaa <709319582@qq.com>
2021-03-24 13:14:39 +1100

    [Update] Insert v-viewer and add breadcrumb

--- vicflora-frontend/src/components/Taxon-classification-component/Taxon-classification-component.vue
+++ vicflora-frontend/src/components/Taxon-classification-component/Taxon-classification-component.vue
@@ -3,152 +3,207 @@
 </style>
 
 <template>
-    <div>
-        <ApolloQuery
-            :query="require('../../graphql/queries/taxonConcept.gql')"
-            :variables="{ id }"
-        >
-            <template v-slot="{ result: { loading, error, data } }">
-            <!-- Loading -->
-            <div v-if="loading" class="loading apollo">Loading...</div>
+  <div>
+    <ApolloQuery
+      :query="require('../../graphql/queries/taxonConcept.gql')"
+      :variables="{ id }"
+    >
+      <template v-slot="{ result: { loading, error, data } }">
+        <!-- Loading -->
+        <div v-if="loading" class="loading apollo">Loading...</div>
 
-            <!-- Error -->
-            <div v-else-if="error" class="error apollo">{{error}}</div>
+        <!-- Error -->
+        <div v-else-if="error" class="error apollo">{{ error }}</div>
 
-            <!-- Result -->
-            <div v-else-if="data" class="result apollo">
-                <!-- higherClass -->
-                <b-row 
-                v-for="(higherItem,index) in data.taxonConcept.higherClassification.slice().sort((a, b) => (a.depth - b.depth))" 
-                :key="higherItem.id" 
-                class="justify-content-md-left m-row">
-                    <b-col cols="2" class="text-left">
-                            <span class="m-taxon-treedefitem-name">
-                            {{ higherItem.taxonConcept.taxonTreeDefItem.name }}
-                        </span>
-                    </b-col>
-                    <div :style="`margin-left:${index*3}vw;`" class="text-left">
-                        
-                        <a :href="'/flora/classification/taxon/' + higherItem.taxonConcept.id">
-                            <span class="m-taxon-name"
-                                :style="higherItem.taxonConcept.taxonTreeDefItem.name === 'genus'|| higherItem.taxonConcept.taxonTreeDefItem.name ==='species'||higherItem.taxonConcept.taxonTreeDefItem.name ==='subspecies'? 'font-style:italic;':'font-style:normal;'"
-                            >{{ higherItem.taxonConcept.taxonName.fullName}}</span>&nbsp;
-                            <span class="m-author">{{ higherItem.taxonConcept.taxonName.authorship}}</span>
-                        </a> 
-                        
-                    </div>
-                </b-row>
-                <b-row>
-                    <b-col class="text-right">
-                        <b-icon icon="caret-up-fill"></b-icon>
-                        <span> Higher taxa</span>
-                    </b-col>
-                </b-row>
-                <hr class="m-hr-top" />
-
-                <!-- Item -->
-                    <b-row class="justify-content-md-left">
-                    <b-col cols="1" class="text-left">
-                        <span class="m-taxon-treedefitem-name">
-                            {{ data.taxonConcept.taxonTreeDefItem.name }}
-                        </span>
-                    </b-col>
-                    <div :style="`margin-left:${(data.taxonConcept.higherClassification.length+1)*3}vw;`" class="text-left">
-                        <a :href="'/flora/classification/taxon/'+ data.taxonConcept.id">
-                            <span class="m-taxon-name"
-                                 :style="data.taxonConcept.taxonTreeDefItem.name==='genus'||data.taxonConcept.taxonTreeDefItem.name==='species'||data.taxonConcept.taxonTreeDefItem.name==='subspecies'? 'font-style:italic;':'font-style:normal;'"
-                            >{{data.taxonConcept.taxonName.fullName}}</span>&nbsp;    
-                            <span class="m-author">{{ data.taxonConcept.taxonName.authorship}}</span>
-                        </a>                     
-                    </div>
-                </b-row>
-                <hr class="m-hr-bottom" />
-                    <b-row>
-                    <b-col class="text-right">
-                        <b-icon icon="caret-down-fill"></b-icon>
-                        <span> Subordinate taxa</span>
-                    </b-col>
-                </b-row>
-                <!-- Child class -->
-                <b-row 
-                    v-for="childItem in data.taxonConcept.children.slice().sort((a, b) => {
-                        let fa = a.taxonName.fullName.toLowerCase(),
-                            fb = b.taxonName.fullName.toLowerCase();
-
-                        if (fa < fb) {
-                            return -1;
-                        }
-                        if (fa > fb) {
-                            return 1;
-                        }
-                        return 0;
-                    })"
-                    :key="childItem.id" class="justify-content-md-left m-row">
-                    <b-col cols="1" class="text-left">
-                            <span class="m-taxon-treedefitem-name">
-                            {{ childItem.taxonTreeDefItem.name }}
-                        </span>
-                    </b-col>
-                    <div :style="`margin-left:${(data.taxonConcept.higherClassification.length+2)*3}vw;`" class="text-left">
-                        <b-link :to="'/flora/classification/taxon/' + childItem.id">
-                            <span class="m-taxon-name"
-                                :style="childItem.taxonTreeDefItem.name==='genus'||childItem.taxonTreeDefItem.name==='species'||childItem.taxonTreeDefItem.name==='subspecies'? 'font-style:italic;':'font-style:normal;'"
-                            >{{ childItem.taxonName.fullName }}</span>&nbsp;    
-                            <span class="m-author">{{childItem.taxonName.authorship}}</span>
-                        </b-link>
-                        
-                    </div>
-                </b-row>
-                
+        <!-- Result -->
+        <div v-else-if="data" class="result apollo">
+          <!-- higherClass -->
+          <b-row
+            v-for="(higherItem,
+            index) in data.taxonConcept.higherClassification
+              .slice()
+              .sort((a, b) => a.depth - b.depth)"
+            :key="higherItem.id"
+            class="justify-content-md-left m-row"
+          >
+            <b-col cols="2" class="text-left">
+              <span class="m-taxon-treedefitem-name">
+                {{ higherItem.taxonConcept.taxonTreeDefItem.name }}
+              </span>
+            </b-col>
+            <div :style="`margin-left:${index * 3}vw;`" class="text-left">
+              <a
+                :href="
+                  '/flora/classification/taxon/' + higherItem.taxonConcept.id
+                "
+              >
+                <span
+                  class="m-taxon-name"
+                  :style="
+                    higherItem.taxonConcept.taxonTreeDefItem.name === 'genus' ||
+                    higherItem.taxonConcept.taxonTreeDefItem.name ===
+                      'species' ||
+                    higherItem.taxonConcept.taxonTreeDefItem.name ===
+                      'subspecies'||
+                    higherItem.taxonConcept.taxonTreeDefItem.name === 'variety'
+                      ? 'font-style:italic;'
+                      : 'font-style:normal;'
+                  "
+                  >{{ higherItem.taxonConcept.taxonName.fullName }}</span
+                >&nbsp;
+                <span class="m-author">{{
+                  higherItem.taxonConcept.taxonName.authorship
+                }}</span>
+              </a>
             </div>
+          </b-row>
+          <b-row>
+            <b-col class="text-right">
+              <b-icon icon="caret-up-fill"></b-icon>
+              <span> Higher taxa</span>
+            </b-col>
+          </b-row>
+          <hr class="m-hr-top" />
 
-            <!-- No result -->
-            <div v-else class="no-result apollo text-left">Loading...No result</div>
-            </template>
-        </ApolloQuery>
-    </div>
+          <!-- Item -->
+          <b-row class="justify-content-md-left">
+            <b-col cols="1" class="text-left">
+              <span class="m-taxon-treedefitem-name">
+                {{ data.taxonConcept.taxonTreeDefItem.name }}
+              </span>
+            </b-col>
+            <div
+              :style="
+                `margin-left:${(data.taxonConcept.higherClassification.length +
+                  1) *
+                  3}vw;`
+              "
+              class="text-left"
+            >
+              <a :href="'/flora/classification/taxon/' + data.taxonConcept.id">
+                <span
+                  class="m-taxon-name"
+                  :style="
+                    data.taxonConcept.taxonTreeDefItem.name === 'genus' ||
+                    data.taxonConcept.taxonTreeDefItem.name === 'species' ||
+                    data.taxonConcept.taxonTreeDefItem.name === 'subspecies' ||
+                    data.taxonConcept.taxonTreeDefItem.name === 'variety'
+                      ? 'font-style:italic;'
+                      : 'font-style:normal;'
+                  "
+                  >{{ data.taxonConcept.taxonName.fullName }}</span
+                >&nbsp;
+                <span class="m-author">{{
+                  data.taxonConcept.taxonName.authorship
+                }}</span>
+              </a>
+            </div>
+          </b-row>
+          <hr class="m-hr-bottom" />
+          <b-row>
+            <b-col class="text-right">
+              <b-icon icon="caret-down-fill"></b-icon>
+              <span> Subordinate taxa</span>
+            </b-col>
+          </b-row>
+          <!-- Child class -->
+          <b-row
+            v-for="childItem in data.taxonConcept.children
+              .slice()
+              .sort((a, b) => {
+                let fa = a.taxonName.fullName.toLowerCase(),
+                  fb = b.taxonName.fullName.toLowerCase();
+
+                if (fa < fb) {
+                  return -1;
+                }
+                if (fa > fb) {
+                  return 1;
+                }
+                return 0;
+              })"
+            :key="childItem.id"
+            class="justify-content-md-left m-row"
+          >
+            <b-col cols="1" class="text-left">
+              <span class="m-taxon-treedefitem-name">
+                {{ childItem.taxonTreeDefItem.name }}
+              </span>
+            </b-col>
+            <div
+              :style="
+                `margin-left:${(data.taxonConcept.higherClassification.length +
+                  2) *
+                  3}vw;`
+              "
+              class="text-left"
+            >
+              <b-link :to="'/flora/classification/taxon/' + childItem.id">
+                <span
+                  class="m-taxon-name"
+                  :style="
+                    childItem.taxonTreeDefItem.name === 'genus' ||
+                    childItem.taxonTreeDefItem.name === 'species' ||
+                    childItem.taxonTreeDefItem.name === 'subspecies' ||
+                    childItem.taxonTreeDefItem.name === 'variety'
+                      ? 'font-style:italic;'
+                      : 'font-style:normal;'
+                  "
+                  >{{ childItem.taxonName.fullName }}</span
+                >&nbsp;
+                <span class="m-author">{{
+                  childItem.taxonName.authorship
+                }}</span>
+              </b-link>
+            </div>
+          </b-row>
+        </div>
+
+        <!-- No result -->
+        <div v-else class="no-result apollo text-left">Loading...No result</div>
+      </template>
+    </ApolloQuery>
+  </div>
 </template>
 
 <script>
-import gql from 'graphql-tag'
+import gql from "graphql-tag";
 export default {
-    name: "TaxonClassificationComponent",
-    data(){
-        return {
-            taxonConcept: '',
-            higherItemNum:0,
-            id: 32065,
-            indentNum:0,
-            
+  name: "TaxonClassificationComponent",
+  data() {
+    return {
+      taxonConcept: "",
+      higherItemNum: 0,
+      id: 32065,
+      indentNum: 0,
+    };
+  },
+  //Apollo-specific options
+  fragments: {
+    taxonConceptFields: gql`
+      fragment taxonConceptFields on TaxonConcept {
+        id
+        guid
+        taxonName {
+          id
+          fullName
+          authorship
         }
+      }
+    `,
+  },
+  computed: {
+    urlId: function() {
+      return this.$route.params.id;
     },
-    //Apollo-specific options
-    fragments: {
-        taxonConceptFields: gql`
-            fragment taxonConceptFields on TaxonConcept {
-                id
-                guid
-                taxonName {
-                    id
-                    fullName
-                    authorship
-                }
-            }`
+  },
+  watch: {
+    urlId: {
+      immediate: true,
+      handler: function(val) {
+        this.id = val;
+      },
     },
-    computed: {
-        urlId: function(){
-            return this.$route.params.id
-        }
-    },
-    watch: {
-        urlId : {
-            immediate: true,
-            handler: function (val) {
-                        this.id = val
-                    },
-        },
-    }
-    
-
-}
+  },
+};
 </script>
--- vicflora-frontend/src/components/Taxon/Taxon.vue
+++ vicflora-frontend/src/components/Taxon/Taxon.vue
@@ -1,9 +1,11 @@
 <style lang="scss" scoped>
-@import "./Taxon.scss";
+.taxon::v-deep {
+  @import "./Taxon.scss";
+}
 </style>
 
 <template>
-  <div>
+  <div class="taxon">
     <b-container>
       <ApolloQuery
         :query="require('../../graphql/queries/taxonConcept-description.gql')"
@@ -19,14 +21,64 @@
           <!-- Result -->
           <div v-else-if="data" class="result apollo">
             <!-- Breadcrumb -->
-            <b-row>
-              Breadcrumb
+            <b-row> 
+              <b-col>
+                <b-breadcrumb>
+                  <!-- higherClassification -->
+                  <b-breadcrumb-item 
+                    v-for="higherItem in data.taxonConcept.higherClassification.slice().sort((a, b) => a.depth - b.depth)" 
+                    :key="higherItem.id"
+                    :href="'/flora/classification/taxon/' + higherItem.taxonConcept.id">{{higherItem.taxonConcept.taxonName.fullName}}
+                  </b-breadcrumb-item>
+                  <!-- Siblings -->
+                  <b-breadcrumb-item>
+                    <div class="m-breadcrumb-selector-item">
+                      <b-form-select v-model="urlId" size="sm">
+                        <template #first>
+                          <b-form-select-option :value="null" disabled>Select sibling...</b-form-select-option>
+                        </template>
+                        <b-form-select-option 
+                          v-for="siblingItem in data.taxonConcept.siblings"
+                          :key="siblingItem.id"
+                          :value="siblingItem.id"
+                        >{{siblingItem.taxonName.fullName}}</b-form-select-option>
+                      </b-form-select>
+                    </div>
+                  </b-breadcrumb-item>
+                  <!-- Children -->
+                   <b-breadcrumb-item>
+                    <div class="m-breadcrumb-selector-item">
+                      <b-form-select v-model="childrenSelected" size="sm">
+                        <template #first>
+                          <b-form-select-option :value="null" disabled>Select child...</b-form-select-option>
+                        </template>
+                        <b-form-select-option 
+                          v-for="childItem in data.taxonConcept.children"
+                          :key="childItem.id"
+                          :value="childItem.id"
+                        >{{childItem.taxonName.fullName}}</b-form-select-option>
+                      </b-form-select>
+                    </div>
+                  </b-breadcrumb-item>
+                </b-breadcrumb>
+              </b-col>
             </b-row>
             <!-- Name -->
             <b-row>
               <b-col class="text-left">
                 <div>
-                  <h2 class="m-name">
+                  <h2
+                    class="m-name"
+                    :style="
+                      data.taxonConcept.taxonTreeDefItem.name === 'genus' ||
+                      data.taxonConcept.taxonTreeDefItem.name === 'species' ||
+                      data.taxonConcept.taxonTreeDefItem.name ===
+                        'subspecies' ||
+                      data.taxonConcept.taxonTreeDefItem.name === 'variety'
+                        ? 'font-style:italic;'
+                        : 'font-style:normal;'
+                    "
+                  >
                     {{ data.taxonConcept.taxonName.fullName }}
                   </h2>
                 </div>
@@ -68,10 +120,10 @@
               class="m-row"
               v-if="
                 data.taxonConcept.taxonTreeDefItem.name !== 'family' &&
-                  data.taxonConcept.taxonTreeDefItem.name !== 'genus' &&
-                  data.taxonConcept.taxonTreeDefItem.name !== 'species' &&
-                  data.taxonConcept.taxonTreeDefItem.name !== 'subspecies' &&
-                  data.taxonConcept.taxonTreeDefItem.name !== 'variety'
+                data.taxonConcept.taxonTreeDefItem.name !== 'genus' &&
+                data.taxonConcept.taxonTreeDefItem.name !== 'species' &&
+                data.taxonConcept.taxonTreeDefItem.name !== 'subspecies' &&
+                data.taxonConcept.taxonTreeDefItem.name !== 'variety'
               "
             >
               <b-col class="text-left">
@@ -79,60 +131,120 @@
                 <TaxonClassificationComponent></TaxonClassificationComponent>
               </b-col>
             </b-row>
-
+            <!-- Tabs -->
             <b-tabs v-model="tabIndex" content-class="mt-4 mb-3 m-row" v-else>
-                <b-tab title="Overview">
-                    <b-row>
-                        <b-col>
-                            <!-- Description -->
-                            <b-row class="m-row">
-                            <!-- text -->
-                            <b-col
-                                class="text-left"
-                                v-if="data.taxonConcept.currentProfile"
-                            >
-                                <div
-                                v-html="data.taxonConcept.currentProfile.profile"
-                                ></div>
-                                <!-- Source -->
-                                <div class="m-row">
-                                    <span class="m-description-title">Source: </span>
-                                    <span>
-                                        
-                                    </span>
-                                </div>
-                                <!-- Created time -->
-                                <div class="m-row">
-                                    <!-- Created info -->
-                                    <div>
-                                        <span class="m-description-title">Created by: </span>
-                                        <span>{{`${data.taxonConcept.createdBy.name}, ${data.taxonConcept.createdAt.slice(0,10)}`}}</span>
-                                    </div>
-                                    <!-- Updated info -->
-                                    <div>
-                                        <span class="m-description-title">Updated by: </span>
-                                        <span>{{`${data.taxonConcept.modifiedBy.name}, ${data.taxonConcept.updatedAt.slice(0,10)}`}}</span>
-                                    </div>
-                                </div>
-                                <div class="m-row" v-if="data.taxonConcept.taxonTreeDefItem.name !== 'species'">
-                                    <b-button variant="primary">Key to the {{getBtnText(data.taxonConcept.taxonTreeDefItem.name)}} of {{data.taxonConcept.taxonName.fullName}}</b-button>
-                                </div>
-                            </b-col>
-                            <!-- img -->
-                            <b-col md="4">
-                                <a>
-                                    <b-img @click="tabIndex++" thumbnail fluid :src="'https://data.rbg.vic.gov.au/cip/'+data.taxonConcept.heroImage.previewUrl" alt="Hero Image"></b-img>
-                                </a>
-                            </b-col>
-                            </b-row>
-                        </b-col>
+              <b-tab title="Overview" active>
+                <b-row>
+                  <b-col>
+                    <!-- Description -->
+                    <b-row class="m-row">
+                      <!-- text -->
+                      <!-- use .replaceAll() to replace the tags in HTML -->
+                      <b-col
+                        class="text-left"
+                        v-if="data.taxonConcept.currentProfile"
+                      >
+                        <div
+                          v-html="data.taxonConcept.currentProfile.profile"
+                        ></div>
+                        <!-- Source -->
+                        <div class="m-row">
+                          <span class="m-description-title">Source: </span>
+                          <span> </span>
+                        </div>
+                        <!-- Created time -->
+                        <div class="m-row">
+                          <!-- Created info -->
+                          <div>
+                            <span class="m-description-title"
+                              >Created by:
+                            </span>
+                            <span>{{
+                              `${
+                                data.taxonConcept.createdBy.name
+                              }, ${data.taxonConcept.createdAt.slice(0, 10)}`
+                            }}</span>
+                          </div>
+                          <!-- Updated info -->
+                          <div>
+                            <span class="m-description-title"
+                              >Updated by:
+                            </span>
+                            <span>{{
+                              `${
+                                data.taxonConcept.modifiedBy.name
+                              }, ${data.taxonConcept.updatedAt.slice(0, 10)}`
+                            }}</span>
+                          </div>
+                        </div>
+                        <div
+                          class="m-row"
+                          v-if="
+                            data.taxonConcept.taxonTreeDefItem.name !==
+                            'species'
+                          "
+                        >
+                          <b-button variant="primary"
+                            >Key to the
+                            {{
+                              getBtnText(
+                                data.taxonConcept.taxonTreeDefItem.name
+                              )
+                            }}
+                            of
+                            {{ data.taxonConcept.taxonName.fullName }}</b-button
+                          >
+                        </div>
+                      </b-col>
+                      <!-- img -->
+                      <b-col md="4">
+                        <a href="#">
+                          <b-img
+                            @click="() => tabIndex++"
+                            thumbnail
+                            fluid
+                            :src="
+                              'https://data.rbg.vic.gov.au/cip/' +
+                              data.taxonConcept.heroImage.previewUrl
+                            "
+                            alt="Hero Image"
+                          ></b-img>
+                        </a>
+                      </b-col>
                     </b-row>
-                </b-tab>
-                <b-tab title="Images" lazy><b-alert show>I'm lazy mounted too!</b-alert></b-tab>           
-                <b-tab title="Specimen Images" lazy><b-alert show>I'm lazy mounted too!</b-alert></b-tab>
-                <b-tab title="Classification" lazy><b-alert show>I'm lazy mounted too!</b-alert></b-tab>
-                </b-tabs>
-
+                  </b-col>
+                </b-row>
+              </b-tab>
+              <b-tab title="Images">
+                   <div class="m-images" v-viewer>
+                        <p v-if="data.taxonConcept.images.length===0">No Images...</p>
+                        <b-img
+                            v-else
+                            class="m-image"
+                            v-for="image in data.taxonConcept.images" :key="image.id"
+                            thumbnail
+                            fluid
+                            :src="
+                              'https://data.rbg.vic.gov.au/cip/' +
+                              image.previewUrl
+                            "
+                            :alt="image.id"
+                        ></b-img>              
+                    </div>
+              </b-tab>
+              <b-tab title="Specimen Images"
+                >
+                <div class="m-images" v-viewer>
+                  <p v-if="data.taxonConcept.specimenImages.length===0">No Images...</p>
+                </div>
+                </b-tab
+              >
+              <b-tab title="Classification" lazy
+                >
+                <TaxonClassificationComponent></TaxonClassificationComponent>
+              </b-tab
+              >
+            </b-tabs>
           </div>
 
           <!-- No result -->
@@ -145,6 +257,11 @@
 
 <script>
 import TaxonClassificationComponent from "../Taxon-classification-component/Taxon-classification-component";
+import "viewerjs/dist/viewer.css";
+import Viewer from "v-viewer";
+import Vue from "vue";
+Vue.use(Viewer);
+
 export default {
   name: "Taxon",
   components: {
@@ -154,28 +271,53 @@ export default {
     return {
       id: "32066",
       tabIndex: 1,
+      siblingSelected:this.$route.params.id,
+      childrenSelected:null,
     };
   },
   methods: {
-    getBtnText: function (name){
-                    switch(name){
-                        case 'family': return 'genera';
-                        case 'genus' : return 'species';
-                    }
+    getBtnText: function (name) {
+      switch (name) {
+        case "family":
+          return "genera";
+        case "genus":
+          return "species";
+      }
     },
   },
   computed: {
-    urlId: function() {
-      return this.$route.params.id;
+    urlId: {
+      get:function () {
+          return this.$route.params.id;
+        },
+      set: function(newVal){
+        this.$router.push({
+                  path: "/flora/classification/taxon/"+newVal,
+        });
+      }
     },
   },
   watch: {
     urlId: {
       immediate: true,
-      handler: function(val) {
+      handler: function (val) {
+        this.childrenSelected = null
         this.id = val;
       },
     },
+    childrenSelected:{
+      handler: function(newVal){
+        if(this.childrenSelected !== null){
+          this.$router.push({
+                    path: "/flora/classification/taxon/"+newVal,
+          })
+          window.location.reload();
+        }
+      }   
+    }
   },
+  beforeUpdate:function(){
+    this.childrenSelected = null
+  }
 };
 </script>
