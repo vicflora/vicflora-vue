c6b5d66262261553e0c82566c9ead2a1602ce685
nielsklazenga <Niels.Klazenga@rbg.vic.gov.au>
2021-05-28 23:48:02 +1000

    [enhancement] Align elements home page

--- vicflora-nuxt/pages/Home/index.vue
+++ vicflora-nuxt/pages/Home/index.vue
@@ -7,40 +7,23 @@
     <b-container>
       <b-row>
         <b-col cols="12" class="text-left" lg="6">
-          <b-row>
-            <div class="m-introduction">
-              <b>VICFLORA</b> is a comprehensive and current guide to the wild
-              plants of Victoria. With plant profiles, identification tools and
-              richly illustrated, the Royal Botanic Gardens Victoria provides
-              this resource free-of-charge for land managers, scientists,
-              students and indeed anyone with an interest in Victorian plants.
-            </div>
-          </b-row>
+          <div class="m-introduction">
+            <b>VICFLORA</b> is a comprehensive and current guide to the wild
+            plants of Victoria. With plant profiles, identification tools and
+            richly illustrated, the Royal Botanic Gardens Victoria provides
+            this resource free-of-charge for land managers, scientists,
+            students and indeed anyone with an interest in Victorian plants.
+          </div>
 
-          <b-row>
-            <b-button block class="m-btn" size="lg" variant="primary">
-              <nuxt-link to="/flora/search?q=**" style="display: block;"
-                >Search</nuxt-link
-              ></b-button
-            >
+          <nuxt-link to="/flora/search?q=**" class="btn btn-lg btn-block btn-primary">Search</nuxt-link>
 
-            <b-button block class="m-btn" size="lg" variant="primary"
-              ><nuxt-link
-                to="/flora/classification/6abc498a-70de-11e6-a989-005056b0018f"
-                >Browse classification</nuxt-link
-              ></b-button
-            >
+          <nuxt-link to="/flora/classification/6abc498a-70de-11e6-a989-005056b0018f" class="btn btn-lg btn-block btn-primary">Browse classification</nuxt-link>
 
-            <b-button block class="m-btn" size="lg" variant="primary"
-              ><a
-                href="https://keybase.rbg.vic.gov.au/keys/show/1903"
-                >Keys</a
-              ></b-button
-            >
-          </b-row>
-
-          <Highlights></Highlights>
+          <a href="https://keybase.rbg.vic.gov.au/keys/show/1903" class="btn btn-lg btn-block btn-primary">Keys</a>
+          <highlights></highlights>
         </b-col>
+
+        
         <b-col cols="12" lg="6" align-self="center">
           <Carousel class="m-carousel"></Carousel>
         </b-col>
@@ -58,6 +41,33 @@ export default {
   components: {
     Carousel,
     Highlights,
+  },
+  data() {
+    return {
+      highlights: [
+        {
+          index: 0,
+          img: "https://vicflora.rbg.vic.gov.au/images/home/lucid-logo-icon-150.png",
+          text: "<h5><b><span class=\"m-hightlight-new\">New</span> multi-access keys</b></h5>" +
+              "New multi-access keys to <a href=\"/keys/fabaceae\">Fabaceae</a> (excl. Acacia), " +
+              "<a href=\"/keys/cyperaceae\">Cyperaceae</a> and " + 
+              "<a href=\"/keys/juncaceae\">Juncaceae</a> in Victoria."
+        },
+        {
+          index: 1,
+          img: "https://vicflora.rbg.vic.gov.au/images/home/eucalcom.jpg",
+          text: "<b><a href=\"keys/eucalypts\">Multi-access key to the Eucalypts</a></b>" +
+              "<p>Check out our new multi-access key to the 159 species and infraspecific taxa of <i>Eucalyptus</i>, <i>Angophora</i> and <i>Corymbia</i> in Victoria.</p>"
+        },
+        {
+          index: 2,
+          img: "https://vicflora.rbg.vic.gov.au/images/home/microwal.jpg",
+          text: "<b><a href=\"/keys/asteraceae\">Multi-access key to the Asteraceae</a></b>" +
+              "<p>Check out our new multi-access key to the 618 species and infraspecific taxa of Asteraceae in Victoria.</p>"
+        }
+      ]
+    }
   }
+
 };
 </script>
