a0fdba6e787c88547d6a4e3b05e713c46fb56443
Zhiyao Li <l709319582@gmail.com>
2021-06-16 08:56:16 +1000

    [temp]

--- vicflora-nuxt/components/Taxon/TaxonTabDistributionMap.vue
+++ vicflora-nuxt/components/Taxon/TaxonTabDistributionMap.vue
@@ -11,61 +11,61 @@
       </div>
       <div v-else-if="data" class="result apollo"> -->
 
+  <div class="m-map" @click="getLayer">
+    <client-only class="m-map">
+      <l-map :zoom="zoom" :center="center" @click="getCoordinate">
+        <l-control-layers position="topright"></l-control-layers>
 
-        <div class="m-map" @click="getLayer">
-          <client-only class="m-map">
-            <l-map :zoom="zoom" :center="center">
-              <l-control-layers position="topright"></l-control-layers>
+        <l-tile-layer
+          url="https://cartodb-basemaps-b.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+        ></l-tile-layer>
 
-              <l-tile-layer
-                url="https://cartodb-basemaps-b.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
-              ></l-tile-layer>
+        <!-- <l-polygon :lat-lngs="data.taxonConcept.bioregions.geometry.coordinates" color="green"></l-polygon> -->
 
-              <!-- <l-polygon :lat-lngs="data.taxonConcept.bioregions.geometry.coordinates" color="green"></l-polygon> -->
+        <l-lwms-tile-layer name="None" base-url="" layer-type="base" />
 
-              
-              <l-lwms-tile-layer 
-                name="None" 
-                base-url="" 
-                layer-type="base"/>
+        <l-marker :lat-lng="markerLatLng">
+            <l-popup>{{this.markerLatLng}}</l-popup>
+        </l-marker>
 
-              <l-lwms-tile-layer
-                v-for="layer in layers"
-                :key="layer.name"
-                :base-url="baseUrl"
-                :visible="layer.visible"
-                :name="layer.name"
-                :layers="layer.layers"
-                :transparent="layer.transparent"
-                :service="layer.service"
-                :version="layer.version"
-                :request="layer.request"
-                :srs="layer.srs"
-                :format="layer.format"
-                :styles="layer.styles"
-                layer-type="base"
-              />
+        <l-lwms-tile-layer
+          v-for="layer in layers"
+          :key="layer.name"
+          :base-url="baseUrl"
+          :visible="layer.visible"
+          :name="layer.name"
+          :layers="layer.layers"
+          :transparent="layer.transparent"
+          :service="layer.service"
+          :version="layer.version"
+          :request="layer.request"
+          :srs="layer.srs"
+          :format="layer.format"
+          :styles="layer.styles"
+          layer-type="base"
+        >
+          
+        </l-lwms-tile-layer>
 
-              <!-- occurences layers -->
-              <l-lwms-tile-layer
-                :base-url="occurrencesUrl"
-                :visible="occurrencesLayer.visible"
-                :name="occurrencesLayer.name"
-                :layers="occurrencesLayer.layers"
-                :transparent="occurrencesLayer.transparent"
-                :service="occurrencesLayer.service"
-                :version="occurrencesLayer.version"
-                :request="occurrencesLayer.request"
-                :srs="occurrencesLayer.srs"
-                :format="occurrencesLayer.format"
-                layer-type="overlay"
-              ></l-lwms-tile-layer>
-            </l-map>
-          </client-only>
-        </div>
+        <!-- occurences layers -->
+        <l-lwms-tile-layer
+          :base-url="occurrencesUrl"
+          :visible="occurrencesLayer.visible"
+          :name="occurrencesLayer.name"
+          :layers="occurrencesLayer.layers"
+          :transparent="occurrencesLayer.transparent"
+          :service="occurrencesLayer.service"
+          :version="occurrencesLayer.version"
+          :request="occurrencesLayer.request"
+          :srs="occurrencesLayer.srs"
+          :format="occurrencesLayer.format"
+          layer-type="overlay"
+        ></l-lwms-tile-layer>
+      </l-map>
+    </client-only>
+  </div>
 
-
-      <!-- </div>
+  <!-- </div>
       <div v-else class="no-result apollo">
         <div class="spinner-border mt-5 mb-1" role="status"></div>
         <h5>Loading...</h5>
@@ -79,6 +79,9 @@ export default {
   props: ["id"],
   data() {
     return {
+      latitude: 0,
+      longitude: 0,
+      markerLatLng:[0,0],
       layer: "Bioregions",
       zoom: 7,
       center: [-36.55, 145.2],
@@ -154,17 +157,27 @@ export default {
       return `https://data.rbg.vic.gov.au/geoserver/vicflora-mapper/wms?cql_filter=${this.occurrencesCql}`;
     }
   },
-  methods:{
+  methods: {
     getLayer(event) {
       if (event.target.labels) {
         // console.log(event.target.labels[0].innerText.trim());
-        if(event.target.labels[0].innerText.trim()!=="Occurrences"){
-          this.layer = event.target.labels[0].innerText.trim()
-          this.$emit('layer', this.layer)
+        if (event.target.labels[0].innerText.trim() !== "Occurrences") {
+          this.layer = event.target.labels[0].innerText.trim();
+          this.$emit("layer", this.layer);
         }
       }
-    }
+    },
+    getCoordinate: function(event) {
+      //{lat: -34.64620136014536, lng: 141.61376953125003}
+      this.addmarker(event.latlng);
+      this.latitude = event.latlng.lat;
+      this.longitude = event.latlng.lng;
+    },
+    addmarker: function({ lat, lng }) {
+      this.markerLatLng = [lat, lng];
+    },
   },
+
 };
 </script>
 
