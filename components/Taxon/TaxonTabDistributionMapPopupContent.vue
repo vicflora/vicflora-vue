1ed803c6714753caf2ec743ab83cb25201a900ad
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2022-01-06 14:53:56 +1100

    Differentiate between AVH and VBA records in map popup content

--- components/Taxon/TaxonTabDistributionMapPopupContent.vue
+++ components/Taxon/TaxonTabDistributionMapPopupContent.vue
@@ -2,33 +2,44 @@
   <div class="m-popup">
     <div v-if="occurrences[page].properties !== undefined">
       <h5>{{occurrences[page].properties.scientificName}}</h5>
-      <p><b>Catalog Number: </b>{{occurrences[page].properties.catalogNumber}}</p>
+      <p v-if="occurrences[page].properties.dataSource === 'AVH'"><b>Catalog Number: </b>{{occurrences[page].properties.catalogNumber}}</p>
+      <p v-else><b>Catalog Number: </b>VBA {{occurrences[page].properties.catalogNumber}}</p>
       <p><b>Occurrence Status: </b>{{occurrences[page].properties.occurrenceStatus}}</p>
       <p><b>Establishment Means: </b>{{occurrences[page].properties.establishmentMeans}}</p>
-      <p class="text-right">
-        <a  
+      <p
+        v-if="occurrences[page].properties.dataSource === 'AVH'"
+        class="text-right">
+        <a
           :href="`https://avh.ala.org.au/occurrences/${occurrences[page].properties.uuid}`"
           target="_blank"
         >AVH record</a>
       </p>
+      <p
+        v-else
+        class="text-right">
+        <a
+          :href="`https://biocache.ala.org.au/occurrences/${occurrences[page].properties.uuid}`"
+          target="_blank"
+        >VBA record</a>
+      </p>
     </div>
-    <div class="m-btn-group"> 
-      <b-icon 
+    <div class="m-btn-group">
+      <b-icon
         v-if="page!==0"
-        icon="arrow-left-circle" 
-        aria-hidden="true" 
-        @click="goPrev" 
-        class="m-btn" 
+        icon="arrow-left-circle"
+        aria-hidden="true"
+        @click="goPrev"
+        class="m-btn"
       />
       <div v-else></div>
       <div v-if="count > 1">{{ `${page + 1} of ${count}` }}</div>
       <div v-else></div>
-      <b-icon 
+      <b-icon
         v-if="page!==count-1"
-        icon="arrow-right-circle" 
-        aria-hidden="true" 
-        @click="goNext" 
-        class="m-btn" 
+        icon="arrow-right-circle"
+        aria-hidden="true"
+        @click="goNext"
+        class="m-btn"
       />
       <div v-else></div>
     </div>
