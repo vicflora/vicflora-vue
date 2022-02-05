b9330da24b53876c31329e59d91773a41f55b6e6
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2022-02-05 15:05:41 +1100

    Format protologue string Fixes #8

--- components/Taxon/TaxonNameProtologue.vue
+++ components/Taxon/TaxonNameProtologue.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="m-protologue">
-    <i>{{ protologue.title }}</i>
-    <b>{{ protologue.volume + ": " }}</b>
-    <span>{{ protologue.pages }}</span>
-  </div>
+  <div class="m-protologue" v-html="protologueCitation"/>
 </template>
 
 <script>
@@ -14,6 +10,25 @@ export default {
       type: Object,
       required: true
     }
+  },
+  computed: {
+    protologueCitation() {
+      let citation = `<i>${this.protologue.title}</i>`
+      if (this.protologue.volume !== undefined && this.protologue.volume) {
+        citation += ` <b>${this.protologue.volume}</b>`
+      }
+      if (this.protologue.pages !== undefined && this.protologue.pages) {
+        citation += `: ${this.protologue.pages}`
+      }
+      if (this.protologue.publicationYear !== undefined
+          && this.protologue.publicationYear) {
+        citation += ` (${this.protologue.publicationYear})`
+      }
+      return citation
+    }
   }
 }
 </script>
+
+
+</style>
