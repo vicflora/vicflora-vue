bad06e2abceb262d2e0431ea84b93aaa273c3c3a
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2022-03-06 18:44:34 +1100

    Add VBA icons to distribution map legend Resolves #6

--- components/Taxon/TaxonTabDistributionLegend.vue
+++ components/Taxon/TaxonTabDistributionLegend.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
-  <b-button 
-    v-b-toggle="'legend'" 
+  <b-button
+    v-b-toggle="'legend'"
     variant="primary"
     size="sm"
   >
@@ -11,38 +11,47 @@
   </b-button>
 
   <b-collapse id="legend">
-    <b-row>
-      <b-col><h4 class="vf-legend-item-group-header"><small>Occurrence status</small></h4></b-col>
-    </b-row>
-    <b-row>
-      <b-col 
-        v-for="item in items.occurrenceStatus"
-        :key="item"
-        md="6" lg="4" xl="3"
-      >
-        <div class="vf-legend-item">
-          <span :class="`vf-legend-item-area vf-legend-item-area-${item}`">
-            <span :class="`vf-legend-item-point vf-legend-item-point-${item}`">&nbsp;</span>
-          </span> <span class="vf-legend-item-label">{{ item.charAt(0).toUpperCase() + item.slice(1) }}</span>
-        </div>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col><h4 class="vf-legend-item-group-header"><small>Establishment means</small></h4></b-col>
-    </b-row>
-    <b-row>
-      <b-col 
-        v-for="item in items.establishmentMeans"
-        :key="item"
-        md="6" lg="4" xl="3"
-      >
-        <div class="vf-legend-item">
-          <span :class="`vf-legend-item-area vf-legend-item-area-${item}`">
-            <span :class="`vf-legend-item-point vf-legend-item-point-${item}`">&nbsp;</span>
-          </span> <span class="vf-legend-item-label">{{ item.charAt(0).toUpperCase() + item.slice(1) }}</span>
-        </div>
-      </b-col>
-    </b-row>
+
+    <table
+      class="table table-borderless text-center">
+      <thead>
+        <tr>
+          <th scope="col"></th>
+          <th scope="col" colspan="3">Origin and degree of establishment</th>
+          <th scope="col">Occurrence status</th>
+        </tr>
+        <tr>
+          <th scope="col" width="*"></th>
+          <th scope="col" width="20%">Native</th>
+          <th scope="col" width="20%">Introduced</th>
+          <th scope="col" width="20%">Cultivated</th>
+          <th scope="col" width="20%">Extinct</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row" class="text-left">AVH</th>
+          <td class="vf-legend-item"><span class="vf-legend-item-point vf-legend-item-point-native">&nbsp;</span></td>
+          <td class="vf-legend-item"><span class="vf-legend-item-point vf-legend-item-point-introduced">&nbsp;</span></td>
+          <td class="vf-legend-item"><span class="vf-legend-item-point vf-legend-item-point-cultivated">&nbsp;</span></td>
+          <td class="vf-legend-item"><span class="vf-legend-item-point vf-legend-item-point-extinct">&nbsp;</span></td>
+        </tr>
+        <tr>
+          <th scope="row" class="text-left">VBA</th>
+          <td class="vf-legend-item"><span class="vf-legend-item-point vf-legend-item-point-vba-native">&nbsp;</span></td>
+          <td class="vf-legend-item"><span class="vf-legend-item-point vf-legend-item-point-vba-introduced">&nbsp;</span></td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <th scope="row" class="text-left">Regions/areas</th>
+          <td class="vf-legend-item"><span class="vf-legend-item-area vf-legend-item-area-native">&nbsp;</span></td>
+          <td class="vf-legend-item"><span class="vf-legend-item-area vf-legend-item-area-introduced">&nbsp;</span></td>
+          <td class="vf-legend-item"><span class="vf-legend-item-area vf-legend-item-area-cultivated">&nbsp;</span></td>
+          <td class="vf-legend-item"><span class="vf-legend-item-area vf-legend-item-area-extinct">&nbsp;</span></td>
+        </tr>
+      </tbody>
+    </table>
   </b-collapse>
 </div>
 </template>
@@ -77,78 +86,91 @@ export default {
 #legend {
   margin: 10px 0;
   border: solid 1px #cccccc;
-}
 
-.vf-legend-item-group-header {
-  margin: 0 0.3rem;
-}
+  .table {
+    margin-bottom: 0;
 
-.vf-legend-item {
-  vertical-align: middle;
-  padding: 0.3rem;
-
-  .vf-legend-item-area {
-    display: inline-block;
-    position: relative;
-    height: 1.5rem;
-    width: 2.5rem;
-    border: solid 1px #cccccc;
-
-    &.vf-legend-item-area-native {
-      background-color: #a4f27d;
+    thead {
+      border-bottom: solid 1px #ccc;
     }
 
-    &.vf-legend-item-area-introduced {
-      background-color: #ffcccc;
+    tr>th:first-child,
+    tr:first-child>th:nth-child(2),
+    tr:nth-child(2)>th:nth-child(4),
+    tr>td:nth-child(4) {
+      border-right: solid 1px #ccc;
     }
 
-    &.vf-legend-item-area-cultivated {
-      background-color: rgba(51, 51, 255, 0.4);
+    .vf-legend-item {
+      vertical-align: middle;
+      padding: 0.3rem;
+
+      .vf-legend-item-area {
+        display: inline-block;
+        position: relative;
+        height: 1.5rem;
+        width: 2.5rem;
+        border: solid 1px #cccccc;
+
+        &.vf-legend-item-area-native {
+          background-color: #a4f27d;
+        }
+
+        &.vf-legend-item-area-introduced {
+          background-color: #ffcccc;
+        }
+
+        &.vf-legend-item-area-cultivated {
+          background-color: rgba(51, 51, 255, 0.4);
+        }
+
+        &.vf-legend-item-area-uncertain {
+          background-color: rgba(255, 187, 0, 0.4);
+        }
+
+        &.vf-legend-item-area-extinct {
+          background-color: rgba(233, 170, 255, 0.4);
+        }
+      }
+
+      .vf-legend-item-point {
+        display: inline-block;
+        height: 6px;
+        width: 6px;
+
+        &.vf-legend-item-point-native {
+          background-color: #2F8A26;
+        }
+
+        &.vf-legend-item-point-introduced {
+          background-color: #ff0000;
+        }
+
+        &.vf-legend-item-point-cultivated {
+          background-color: #3333ff;
+        }
+
+        &.vf-legend-item-point-uncertain {
+          background-color: #ffbb00;
+        }
+
+        &.vf-legend-item-point-extinct {
+          background-color: #e9aaff;
+        }
+
+        &.vf-legend-item-point-vba-native {
+          background-color: #b0d619;
+          height: 4px;
+          width: 4px;
+        }
+
+        &.vf-legend-item-point-vba-introduced {
+          background-color: #ea98e9;
+          height: 4px;
+          width: 4px;
+        }
+      }
     }
-
-    &.vf-legend-item-area-uncertain {
-      background-color: rgba(255, 187, 0, 0.4);
-    }
-
-    &.vf-legend-item-area-extinct {
-      background-color: rgba(233, 170, 255, 0.4);
-    }
-  }
-
-  .vf-legend-item-point {
-    display: inline-block;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    -webkit-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    height: 6px;
-    width: 6px;
-
-    &.vf-legend-item-point-native {
-      background-color: #2F8A26;
-    }
-
-    &.vf-legend-item-point-introduced {
-      background-color: #ff0000;
-    }
-
-    &.vf-legend-item-point-cultivated {
-      background-color: #3333ff;
-    }
-
-    &.vf-legend-item-point-uncertain {
-      background-color: #ffbb00;
-    }
-
-    &.vf-legend-item-point-extinct {
-      background-color: #e9aaff;
-    }
-  }
-
-  .vf-legend-item-label {
-    vertical-align: top;
-    margin-left: 0.5rem;
   }
 }
 </style>
