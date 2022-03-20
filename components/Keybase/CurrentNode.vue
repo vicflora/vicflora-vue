6438bc927eaf27d6c88fe6a253450a2142bb46e2
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2022-03-20 18:29:43 +1100

    Make KeyBase player controls visible Resolves #14

--- components/Keybase/CurrentNode.vue
+++ components/Keybase/CurrentNode.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="keybase-player-currentnode">
-    <h3>Current node<span class="keybase-player-menu">
-      <span class="keybase-player-stepback"><a href="" title="Step back"><i class="fa fa-undo fa-lg fa-lg"></i></a></span>
-      <span class="keybase-player-startover"><a href="" title="Start over"><i class="fa fa-refresh fa-lg fa-lg"></i></a></span></span>
+    <h3>Current node
+      <span class="keybase-player-menu">
+        <b-button 
+          variant="outline-primary"
+          size="sm"
+          class="keybase-player-stepback"
+        >
+          <nuxt-link to="#" title="Step back">
+            <font-awesome-icon icon="arrow-rotate-left"/>
+          </nuxt-link>
+        </b-button>
+        <b-button 
+          variant="outline-primary"
+          size="sm"
+          class="keybase-player-startover"
+        >
+          <nuxt-link to="#" title="Start over">
+            <font-awesome-icon icon="rotate"/>
+          </nuxt-link>
+        </b-button>
+      </span>
     </h3>
     <div>
       <ul v-if="currentNode">
