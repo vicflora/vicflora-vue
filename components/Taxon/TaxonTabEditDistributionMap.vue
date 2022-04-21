bddce44f3787733015f76f35bd99e8312b63f8e0
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2022-04-21 13:09:18 +1000

    Start implementation of Distribution Editing form Create editor interface #3

---
+++ components/Taxon/TaxonTabEditDistributionMap.vue
@@ -0,0 +1,245 @@
+<template>
+  <div class="m-map">
+    <client-only class="m-map">
+      <l-map
+        ref="map"
+        :zoom="zoom"
+        :center="center"
+        @click="getOccurrences"
+        :options="mapOptions"
+      >
+        <l-marker v-if="marker" :lat-lng="marker" ref="marker">
+          <l-icon
+            :icon-url="icon.url"
+            :icon-size="icon.size"
+            :shadow-size="icon.size"
+          />
+          <l-popup
+            v-if="!disableMapPopups"
+            ref="popup"
+            :lat-lng="marker"
+            :options="popupOptions"
+          >
+            <taxon-tab-distribution-map-popup-content
+              :occurrences="taxonOccurrencesAtPoint.data"/>
+          </l-popup>
+        </l-marker>
+
+        <l-tile-layer
+          url="https://cartodb-basemaps-b.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+        />
+
+        <l-wms-tile-layer
+          :base-url="occurrenceLayerUrl"
+          :visible="occurrenceLayer.visible"
+          :name="occurrenceLayer.name"
+          :layers="occurrenceLayer.layers"
+          :transparent="occurrenceLayer.transparent"
+          :service="occurrenceLayer.service"
+          :version="occurrenceLayer.version"
+          :request="occurrenceLayer.request"
+          :srs="occurrenceLayer.srs"
+          :format="occurrenceLayer.format"
+          :styles="occurrenceLayer.styles"
+          layer-type="overlay"
+        />
+      </l-map>
+    </client-only>
+  </div>
+</template>
+
+<script>
+import { iconMixin, popupMixin } from "@/mixins/mapMixins"
+import "leaflet/dist/leaflet.css"
+import TaxonTabDistributionMapPopupContent
+    from "@/components/Taxon/TaxonTabDistributionMapPopupContent"
+import { taxonOccurrencesAtPointQuery }
+    from "@/graphql/queries/taxonOccurrencesAtPointQuery.gql"
+import 'leaflet-draw/dist/leaflet.draw.css'
+
+export default {
+  name: "TaxonTabEditDistributionMap",
+  components: {
+    TaxonTabDistributionMapPopupContent
+  },
+  mixins: [
+    iconMixin,
+    popupMixin
+  ],
+  props: {
+    taxonConceptId: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      zoom: 7,
+      marker: null,
+      center: [-36.55, 145.2],
+      occurrenceLayer: {
+        name: "Occurrences",
+        service: "WMS",
+        version: "1.1.0",
+        request: "GetMap",
+        visible: true,
+        layers: "vicflora-mapper:taxon_occurrences",
+        srs: "EPSG:4326",
+        format: "image/png",
+        transparent: true,
+        styles: "vicflora-mapper:vicflora_point",
+      },
+      mapOptions: {
+        name: "Victoria map"
+      },
+      taxonOccurrencesAtPoint: [],
+      disableMapPopups: false,
+    }
+  },
+  computed: {
+    cqlFilter: function() {
+      return `taxon_concept_id='${this.taxonConceptId}' ` +
+          `AND occurrence_status NOT IN ('doubtful', 'absent') ` +
+          `AND establishment_means NOT IN ('cultivated')`
+    },
+    occurrenceLayerUrl: function() {
+      return process.env.geoserverWms +
+          `?cql_filter=${this.cqlFilter}`
+    },
+  },
+  mounted() {
+    this.$emit("layer", this.visibleLayer)
+
+    this.$nextTick(() => {
+      const L = this.$L
+      const map = this.$refs.map.mapObject
+
+      const drawnFeatures = new L.FeatureGroup().addTo(map)
+
+      const shapeOptions = {
+        polylineID: false,
+        color: 'gray',
+        fillColor: 'gray',
+        weight: 2,
+        fillOpacity: 0.5,
+        lineCap: 'round',
+        lineJoin: 'bevel',
+        dashArray: '',
+        opacity: 1,
+      }
+
+      const drawError = {
+        color: 'orange',
+        timeout: 2000,
+        message: '<strong>Not allowed<strong>',
+      }
+
+      const drawControl = new L.Control.Draw({
+        draw: {
+          polyline: false,
+          polygon: {
+            allowIntersection: false,
+            drawError: drawError,
+            showArea: true,
+            metric: true,
+            repeatMode: false,
+            shapeOptions: shapeOptions,
+          },
+          rectangle: {
+            allowIntersection: false,
+            drawError: drawError,
+            showArea: true,
+            metric: true,
+            repeatMode: false,
+            shapeOptions: shapeOptions,
+          },
+          circle: false,
+          marker: false,
+          circlemarker: false,
+        },
+        edit: {
+          featureGroup: drawnFeatures,
+          remove: true,
+        },
+      })
+      // Add all draw shapes on the map
+      map.addControl(drawControl);
+
+      const wktFromGeoJSON = (geojson) => {
+        const type = geojson.geometry.type.toUpperCase()
+        return type + ' (' +
+          geojson.geometry.coordinates.map((ring) => {
+            return '(' + ring.map(p => {
+              return p[0] + ' ' + p[1]
+            }).join(', ') + ')'
+          }).join(', ') + ')'
+      }
+
+      const getPopupContent = (layer) => {
+        let msg = '<div>'
+        const geojson = layer.toGeoJSON()
+        msg += `<pre style="max-height:150px;overflow-y:auto">${JSON.stringify(geojson.geometry, null, 2)}</pre>`
+        const wkt = wktFromGeoJSON(geojson)
+        msg += `<p>${wkt}</p>`
+        msg += '</div>'
+        return msg
+      }
+
+      map.on(L.Draw.Event.DRAWSTART, () => {
+        this.disableMapPopups = true
+      })
+
+      map.on(L.Draw.Event.DRAWSTOP, () => {
+        this.disableMapPopups = false
+      })
+
+      map.on(L.Draw.Event.CREATED, (event) => {
+        const layer = event.layer
+        const content = getPopupContent(layer)
+        if (content !== null) {
+          layer.bindPopup(content)
+        }
+        layer.addTo(map)
+        drawnFeatures.addLayer(layer)
+        layer.openPopup()
+      })
+
+      map.on(L.Draw.Event.EDITED, (event) => {
+        const layers = event.layers
+        let content = null
+        layers.eachLayer((layer) => {
+          content = getPopupContent(layer)
+          if (content !== null) {
+            layer.setPopupContent(content)
+          }
+        })
+      })
+    })
+  },
+  methods: {
+    getOccurrences(event) {
+      this.marker = null
+      let mapClick = event.latlng
+      this.$apollo.addSmartQuery('taxonOccurrencesAtPoint', {
+        query: taxonOccurrencesAtPointQuery,
+        variables: {
+          taxonConceptId: this.taxonConceptId,
+          latitude: mapClick.lat,
+          longitude: mapClick.lng
+        },
+        update(data) {
+          return data.taxonOccurrencesAtPoint
+        },
+        result({ data, loading }) {
+          if (!loading) {
+            if (data.taxonOccurrencesAtPoint.data.length > 0) {
+              this.marker = mapClick
+              this.openPopup()
+            }
+          }
+        }
+      })
+    }
+  }
+}
+</script>
--- plugins/leaflet.client.js
+++ plugins/leaflet.client.js
@@ -1,27 +1,48 @@
 import Vue from 'vue'
-import { LControl, LControlAttribution, LControlLayers, LControlScale,
-  LControlZoom, LIcon, LIconDefault, LImageOverlay, LLayerGroup, LMap,
-  LMarker, LPopup, LTileLayer, LTooltip, LWMSTileLayer } from 'vue2-leaflet'
+import { 
+  LCircle,
+  LCircleMarker,
+  LControl, 
+  LControlAttribution, 
+  LControlLayers, 
+  LControlScale,
+  LControlZoom, 
+  LFeatureGroup,
+  LGeoJson,
+  LIcon, 
+  LIconDefault, 
+  LImageOverlay, 
+  LLayerGroup, 
+  LMap,
+  LMarker, 
+  LPolygon,
+  LPolyline,
+  LPopup, 
+  LRectangle,
+  LTileLayer, 
+  LTooltip, 
+  LWMSTileLayer } from 'vue2-leaflet'
+import LDraw from 'leaflet-draw'
 
-// Vue.component('l-circle', LCircle)
-// Vue.component('l-circle-marker', LCircleMarker)
+Vue.component('l-circle', LCircle)
+Vue.component('l-circle-marker', LCircleMarker)
 Vue.component('l-control', LControl)
 Vue.component('l-control-attribution', LControlAttribution)
 Vue.component('l-control-layers', LControlLayers)
 Vue.component('l-control-scale', LControlScale)
 Vue.component('l-control-zoom', LControlZoom)
-// Vue.component('l-feature-group', LFeatureGroup)
-// Vue.component('l-geo-json', LGeoJson)
+Vue.component('l-feature-group', LFeatureGroup)
+Vue.component('l-geo-json', LGeoJson)
 Vue.component('l-icon', LIcon)
 Vue.component('l-icon-default', LIconDefault)
 Vue.component('l-image-overlay', LImageOverlay)
 Vue.component('l-layer-group', LLayerGroup)
 Vue.component('l-map', LMap)
 Vue.component('l-marker', LMarker)
-// Vue.component('l-polygon', LPolygon)
-// Vue.component('l-polyline', LPolyline)
+Vue.component('l-polygon', LPolygon)
+Vue.component('l-polyline', LPolyline)
 Vue.component('l-popup', LPopup)
-// Vue.component('l-rectangle', LRectangle)
+Vue.component('l-rectangle', LRectangle)
 Vue.component('l-tile-layer', LTileLayer)
 Vue.component('l-tooltip', LTooltip)
 Vue.component('l-wms-tile-layer', LWMSTileLayer)
