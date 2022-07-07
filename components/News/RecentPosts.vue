17a60d8069bf72eb5db3522d80e75998485b9612
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2022-07-07 17:33:27 +1000

    Set up news items

---
+++ components/News/RecentPosts.vue
@@ -0,0 +1,59 @@
+<!--
+ Copyright 2022 Royal Botanic Gardens Board
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
+<template>
+  <div id="recent-posts">
+    <h2>Recent posts</h2>
+    <ul v-if="recentPosts.length">
+      <li 
+        v-for="post in recentPosts"
+        :key="post.path"
+      >
+        <NuxtLink :to="getRoute(post.path)">
+          {{ post.title }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecentPosts",
+  props: {
+    recentPosts: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    getRoute(path) {
+      let to = path.replace('posts', 'news')
+      for (var i=0; i<3; i++) {
+        to = to.replace('-', '/')
+      }
+      return to
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+ul {
+  padding-left: 0;
+  list-style-type: none;
+}
+</style>
---
+++ pages/news/news-item.vue
@@ -0,0 +1,85 @@
+<!--
+ Copyright 2022 Royal Botanic Gardens Board
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
+<template>
+  <article class="container static-content">
+    <header class="m-title">
+      <h1>{{ markdown.title }}</h1>
+    </header>
+    <!-- content -->
+    <BRow>
+      <BCol align-self="start" cols="9" class="text-left">
+        <p>
+          <FontAwesomeIcon icon="calendar-days"/> {{ created }}
+        </p>
+        <nuxt-content
+          id="scrollspy-nested"
+          :document="markdown"
+          class="m-content"
+        />
+      </BCol>
+      <BCol align-self="start" cols="3">
+        <RecentPosts :recentPosts="posts"/>
+      </BCol>
+    </BRow>
+  </article>
+</template>
+
+<router>
+{
+  path: '/news/:year/:month/:day/:slug'
+}
+</router>
+
+<script>
+const RecentPosts = () => import('@/components/News/RecentPosts')
+
+export default {
+  name:"NewsItem",
+  components: {
+    RecentPosts,
+  },
+  async asyncData({ $content, params}) {
+    const markdown = await $content(`posts/${params.year}-${params.month}-${params.day}-${params.slug}`).fetch()
+    const posts = await $content('posts')
+    .only(['title', 'created'])
+    .sortBy('created', 'desc')
+    .limit(5)
+    .fetch()
+
+    return { markdown, posts }
+  },
+  data() {
+    return {
+      content: ""
+    };
+  },
+  computed: {
+    created() {
+      const options = { 
+        weekday: 'long', 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric',
+      }
+
+      const createDate = new Date(this.markdown.created)
+      return createDate.toLocaleString('en-AU', options)
+    }
+  }
+}
+</script>
+
