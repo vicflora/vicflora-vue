7294acbfb996e2e2ce2c1578f8f11edc51699a45
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2022-08-06 19:54:53 +1000

    create filter config. form

---
+++ components/Search/SearchFilterConfigGroup.vue
@@ -0,0 +1,83 @@
+<!--
+ Copyright 2022 Royal Botanic Gardens Board
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
+<template>
+  <BRow>
+    <b-col cols="12"><h4 class="vf-filter-group-label">{{ filterGroup.label }}</h4></b-col>
+    <b-col cols="6">
+      <SearchFilterConfigField 
+        v-for="field in leftColFields"
+        :key="field.name"
+        :facetField="field"
+        :cols="6"
+      />
+    </b-col>
+    <b-col v-if="rightColFields.length" cols="6">
+      <SearchFilterConfigField 
+        v-for="field in rightColFields"
+        :key="field.name"
+        :facetField="field"
+        :cols="6"
+      />
+    </b-col>
+  </BRow>
+</template>
+
+<script>
+const SearchFilterConfigField = () => import('@/components/Search/SearchFilterConfigField')
+
+export default {
+  name: "SearchFilterConfigGroup",
+  components: {
+    SearchFilterConfigField
+  },
+  props: {
+    filterGroup: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    filterFields() {
+      return this.$store.getters['search/getFilterFields'].filter((field) => {
+        return field.group === this.filterGroup.name
+      })
+    },
+    leftColFields() {
+      const middleIndex = Math.ceil(this.filterFields.length / 2)
+      const leftCol = this.filterFields.slice(0, middleIndex)
+      return leftCol
+    },
+    rightColFields() {
+      let rightCol = []
+      if (this.filterFields.length > 1) {
+        const middleIndex = Math.ceil(this.filterFields.length / 2)
+        rightCol = this.filterFields.slice(-(middleIndex-1))
+      }
+      return rightCol
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.vf-filter-group-label {
+  font-size: x-large;
+  margin-top: 1.2rem;
+  margin-bottom: 0.5rem;
+}
+
+</style>
