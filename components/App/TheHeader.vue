014f9b0dc0d6327178fbc42bdf40bbc11d6f3abf
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2023-05-15 17:30:20 +1000

    add outage notice

--- components/App/TheHeader.vue
+++ components/App/TheHeader.vue
@@ -1,5 +1,17 @@
 <template>
   <header>
+    <b-alert
+      :show="true"
+      variant="warning"
+      dismissible
+    >
+      <strong>OUTAGE NOTICE:</strong> On <strong>Wednesday, May 17<sup>th</sup></strong>, at <strong>5 pm
+      AEST</strong> (UTC+10), the Royal Botanic Gardens Victoria network will
+      be down for an upgrade of the firewall and core switch. If everything
+      goes well, Vicflora should be back up in about 20 minutes, but images
+      may take up to two hours to re-appear. We apologise for the inconvenience.
+    </b-alert>
+
     <TheNavigationBar/>
     <TheBanner/>
     <TheProgressBar/>
