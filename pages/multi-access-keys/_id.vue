092dc8873d6bfa35d427c3459d6f41a8990f1cb3
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2023-08-17 03:32:23 +0000

    Remove redirects from 'multi-access-keys' pages to 'matrix-keys' page

--- pages/multi-access-keys/_id.vue
+++ pages/multi-access-keys/_id.vue
@@ -1,12 +1,12 @@
 <!--
  Copyright 2022 Royal Botanic Gardens Board
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
      http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,7 @@
     <div class="container">
       <div class="row">
         <div class="col">
-          <multi-access-key 
+          <multi-access-key
             v-if="multiAccessKey"
             :multiAccessKey="multiAccessKey"
           />
@@ -32,7 +32,7 @@
 <script>
 import MultiAccessKeyQuery from '@/graphql/queries/MultiAccessKeyQuery'
 
-const MultiAccessKey = () => 
+const MultiAccessKey = () =>
     import('@/components/MultiAccessKey/MultiAccessKey')
 
 export default {
@@ -60,15 +60,15 @@ export default {
       skip: true,
     },
   },
-  beforeRouteEnter(to, from, next) {
-    next(vm => {
-      if (!vm.$store.getters['isLoggedIn']) {
-        vm.$router.push({
-          name: 'flora-matrix-keys'
-        })
-      }
-    })
-  },
+  // beforeRouteEnter(to, from, next) {
+  //   next(vm => {
+  //     if (!vm.$store.getters['isLoggedIn']) {
+  //       vm.$router.push({
+  //         name: 'flora-matrix-keys'
+  //       })
+  //     }
+  //   })
+  // },
   created() {
     this.$apollo.queries.multiAccessKey.setVariables({id: this.$route.params.id})
     this.$apollo.queries.multiAccessKey.skip = false
