6807a89ff0167b6731f05bc049c3433af17c525c
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2023-09-23 16:33:28 +1000

    Remove all metadata from head section
    - does not come through in HTML source

--- pages/flora/taxon/_id.vue
+++ pages/flora/taxon/_id.vue
@@ -86,72 +86,12 @@ export default {
     ErrorMessage,
   },
   head() {
-    if (this.taxonConcept) {
-      return {
-        title: this.pageTitle,
-        link: [
-          {
-            rel: "schema.dcterms",
-            href: "http://purl.org/dc/terms/"
-          },
-          {
-            rel: "schema.AGLSTERMS",
-            href: "http://www.agls.gov.au/agls/terms/"
-          }
-        ],
-        meta: [
-          {
-            name: "dcterms.title",
-            content: this.pageTitle
-          },
-          {
-            name: "dcterms.identifier",
-            content: `https://vicflora.rbg.vic.gov.au${this.$route.path}`
-          },
-          {
-            name: "dcterms.creator",
-            content: "scheme=AGLSTERMS.AglsAgent; corporateName=Royal Botanic Gardens Victoria; address=Birdwood Avenue, Melbourne 3004; contact=+61 3 92522300"
-          },
-          {
-            name: "dcterms.publisher",
-            content: "scheme=AGLSTERMS.AglsAgent; corporateName=Royal Botanic Gardens Victoria; address=Birdwood Avenue, Melbourne 3004; contact=+61 3 92522300"
-          },
-          {
-            name: "dcterms.description",
-            content: `Treatment of ${this.taxonConcept.taxonName.fullName}`
-          },
-          {
-            name: "dcterms.created",
-            content: `scheme=dcterms.ISO8601; ${this.taxonConcept.createdAt}`
-          },
-          {
-            name: "dcterms.modified",
-            content: `scheme=dcterms.ISO8601; ${this.taxonConcept.createdAt}`
-          },
-          {
-            name: "dcterms.language",
-            content: "dcterms.RFC4646; en-AU"
-          },
-        ],
-        script: [{ innerHTML: JSON.stringify(this.structuredData), type: 'application/ld+json' }],
-      }
+    return {
+      title: this.pageTitle,
     }
   },
   data() {
     return {
-      structuredData: {
-        "@context": "http://schema.org",
-        "@type": "Webpage",
-        "headline": "",
-        "datePublished": "",
-        "dateModified": "",
-        "publisher": {
-          "@type": "Organization",
-          "name": "Royal Botanic Gardens Victoria",
-          "url": "https://www.rbg.vic.gov.au"
-        },
-        "keywords": [ "botany", "flora", "Australia", "Victoria" ]
-      },
       taxonConcept: null,
       lastSearch: null,
       pageTitle: 'Flora of Victoria',
@@ -166,10 +106,6 @@ export default {
           $nuxt.$emit('progress-bar-stop')
           this.taxonConcept = data.taxonConcept
           this.pageTitle = `VicFlora: ${data.taxonConcept.taxonName.fullName}`
-          this.structuredData.headline = `VicFlora: ${data.taxonConcept.taxonName.fullName}`
-          this.structuredData.datePublished = data.taxonConcept.createdAt
-          this.structuredData.dateModified = data.taxonConcept.updatedAt
-          this.structuredData.keywords.push(data.taxonConcept.taxonName.fullName)
         }
       },
       error(error) {
