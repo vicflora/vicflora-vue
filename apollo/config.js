7dad92088a4f678944d145d4e13402eec3071730
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2023-09-27 14:45:43 +1000

    Minor changes to config.

--- apollo/config.js
+++ apollo/config.js
@@ -1,11 +1,8 @@
 import cache from './cache'
 
 export default function(ctx) {
-  const endpoint = process.env.graphqlEndpoint
-  const token = process.env.token
   return {
-    httpEndpoint: endpoint,
-    // getAuth: () => `Bearer ${token}`,
+    httpEndpoint: '/graphql',
     cache
   }
 }
