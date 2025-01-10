b919457ad69983cd4884026bbbe6f705b07c7cdc
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2025-01-10 12:25:34 +0000

    changed date on alert message

--- components/App/TheAlert.vue
+++ components/App/TheAlert.vue
@@ -8,8 +8,8 @@
     >
       Due to electrical works in the National Herbarium of Victoria there will
       be a power outage that affects almost all services in VicFlora on
-      Tuesday, 24 December, from c. 7:30 am AEDT. We hope to have all services
-      back up by 9 am. We apologise for the inconvenience.
+      Friday, 10 January, from c. 6:30 am AEDT. We hope to have all services
+      back up by 9:30 am. We apologise for the inconvenience.
     </b-alert>
   </div>
 </template>
