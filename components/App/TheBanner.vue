109f6abe11f1b90a4483c9221cc8d10209a8ecd2
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2025-02-10 14:09:58 +0000

    change VicFunga to AusFunga

--- components/App/TheBanner.vue
+++ components/App/TheBanner.vue
@@ -12,8 +12,8 @@
                 />
             </div>
             <div class="m-title-container">
-                <span class="m-title">VicFunga</span>
-                <span class="m-subtitle">Fungi and slime moulds of Victoria</span>  
+                <span class="m-title">AusFunga</span>
+                <span class="m-subtitle">Fungi of Australia</span>  
             </div>                
         </div>
       </a>
