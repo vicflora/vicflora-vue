b6c13cb6f86fab7d1532f0ea2bc0c2a61b01a5a0
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2025-02-25 04:26:09 +0000

    update site to include all bryophyte groups

--- pages/flora/matrix-keys/index.vue
+++ pages/flora/matrix-keys/index.vue
@@ -3,9 +3,29 @@
     <header class="m-title">
       <h1>Multi-access keys</h1>
       <nuxt-content :document="markdown" class="m-content" />
-      <div v-if="pages.length" class="linked-pages">
+      <div v-if="pages_vascular.length" class="linked-pages">
+        <a id="vascular-plants"></a>
+        <h2>Vascular plants</h2>
         <b-row>
-          <b-card-group v-for="page in pages" :key="page.path" class="col-md-6 col-lg-4">
+          <b-card-group v-for="page in pages_vascular" :key="page.path" class="col-md-6 col-lg-4">
+            <b-card :title="page.title" title-tag="h2" :img-src="page.image" img-alt="" img-top class="card-matrix-key">
+              <p>{{ page.description.replace('Introduction\n', '') }}</p>
+              <p>
+                <icon-button-link :to="page.permalink.replace('/pages/', '/flora/matrix-keys/')" icon="play-btn"
+                  text="Lucid key player" />
+              </p>
+              <p>
+                <icon-button-link :to="page.permalink" icon="info-circle" text="Character description" />
+              </p>
+            </b-card>
+          </b-card-group>
+        </b-row>
+      </div>
+      <div v-if="pages_bryophytes.length" class="linked-pages">
+        <a id="bryophytes"></a>
+        <h2>Bryophytes</h2>
+        <b-row>
+          <b-card-group v-for="page in pages_bryophytes" :key="page.path" class="col-md-6 col-lg-4">
             <b-card :title="page.title" title-tag="h2" :img-src="page.image" img-alt="" img-top class="card-matrix-key">
               <p>{{ page.description.replace('Introduction\n', '') }}</p>
               <p>
@@ -40,8 +60,14 @@ export default {
   },
   async asyncData({ $content, params }) {
     const markdown = await $content('pages/multi-access-keys').fetch()
-    const pages = await $content('pages')
-      .where({ display: { $ne: false }, category: 'Multi-access keys' })
+    const pages_vascular = await $content('pages')
+      .where({ display: { $ne: false }, category: 'Multi-access keys vascular plants' })
+      .only(['title', 'description', 'permalink', 'image'])
+      .sortBy('category').sortBy('title')
+      .fetch()
+
+    const pages_bryophytes = await $content('pages')
+      .where({ display: { $ne: false }, category: 'Multi-access keys bryophytes' })
       .only(['title', 'description', 'permalink', 'image'])
       .sortBy('category').sortBy('title')
       .fetch()
@@ -82,21 +108,14 @@ export default {
       pageTitle,
       structuredData,
       markdown,
-      pages,
+      pages_vascular,
+      pages_bryophytes,
     }
   },
   data() {
     return {
       pageTitle: null,
       structuredData: null,
-    }
-  },
-  computed: {
-    subpages() {
-      return this.pages.map((page) => {
-        page.href = page.path
-
-      })
     }
   },
 }
